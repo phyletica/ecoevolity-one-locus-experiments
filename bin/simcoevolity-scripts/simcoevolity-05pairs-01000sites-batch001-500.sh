5fab8d38f01c8bdf70939d47f84aa10d63601216
Jamie R. Oaks <joaks1@gmail.com>
2018-06-23 20:53:32 -0500

    Adding scripts to run simcoevolity.

---
+++ bin/simcoevolity-scripts/simcoevolity-05pairs-01000sites-batch001-500.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source ${PBS_O_HOME}/.bash_profile
+    cd $PBS_O_WORKDIR
+
+    module load gcc/5.3.0
+fi
+
+locussize="1000"
+simname="05pairs-0${locussize}sites"
+cfgpath="../../configs/config-${simname}.yml"
+outputdir="../../simulations/validation/${simname}/batch001"
+rngseed=869845446
+nreps=500
+
+mkdir -p "$outputdir"
+
+simcoevolity --seed="$rngseed" -n "$nreps" -l "$locussize" -o "$outputdir" "$cfgpath"
